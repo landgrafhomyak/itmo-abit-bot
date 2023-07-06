89270e45f53587296aa749719d1f7f6b9ea51dce
Eridan Domoratskiy <eridan200@mail.ru>
2023-07-06 22:57:33 +0300

    Add bot run

--- src/main/kotlin/io/github/landgrafhomyak/itmo/abitbot/AbitBotApplication.kt
+++ src/main/kotlin/io/github/landgrafhomyak/itmo/abitbot/AbitBotApplication.kt
@@ -1,15 +1,18 @@
 package io.github.landgrafhomyak.itmo.abitbot
 
+import com.github.kotlintelegrambot.Bot
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AbitBotApplication : ApplicationRunner {
+class AbitBotApplication(
+    private val bot: Bot,
+) : ApplicationRunner {
 
     override fun run(args: ApplicationArguments) {
-        TODO("Not yet implemented")
+        bot.startPolling()
     }
 }
 
--- src/main/kotlin/io/github/landgrafhomyak/itmo/abitbot/BotConfig.kt
+++ src/main/kotlin/io/github/landgrafhomyak/itmo/abitbot/BotConfig.kt
@@ -1,18 +1,19 @@
 package io.github.landgrafhomyak.itmo.abitbot
 
+import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration("botConfig")
-class Bot(
+@Configuration
+class BotConfig(
     @Value("\${tg.token}")
     private val token: String,
 ) {
 
     @Bean
-    fun bot() = bot {
-        token = this@Bot.token
+    fun bot(): Bot = bot {
+        token = this@BotConfig.token
     }
 }
